a1cea3c7083606b7530289919abf076088674f0c
Tymotex <timzhang3@gmail.com>
2021-09-11 23:13:05 +1000

    Added annotations to heart rate graph

--- src/components/HeartRate/heatRate.js
+++ src/components/HeartRate/heatRate.js
@@ -4,62 +4,85 @@ import { makeStyles } from "@material-ui/core/styles";
 import Chart from "react-apexcharts";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        fontFamily: "Comfortaa",
-        fontWeight: "bolder",
-        fontSize: "20px",
-        color: "#3f50b5",
-        marginLeft: "15px",
-        paddingTop: "10px"
-    },
+  root: {
+    fontFamily: "Comfortaa",
+    fontWeight: "bolder",
+    fontSize: "20px",
+    color: "#3f50b5",
+    marginLeft: "15px",
+    paddingTop: "10px"
+  },
 }));
 
-const date = ["01/09/2021 9:00", "02/09/2021 13:00", "03/09/2021 6:00", "04/09/2021 15:00", "05/09/2021 15:00", "06/09/2021 19:00", "07/09/2021 20:00"];
-const options = {
-  chart: {
-    id: "basic-bar",
-  },
-  xaxis: {
-    categories: Array.from(Array(7).keys()),
-  },
-  tooltip: {
-    x: {
-      formatter: (s) => date[s - 1],
-    },
-  },
-  chart: {
-    toolbar: {
-      tools: {
-        reset: false,
-        pan: false  
-      },
-    },
-  },
-};
-
-const chart = {
-    toolbar: {
-        tools: {
-            reset: false
-        }
-    }
-}
-const series = [
-    {
-        name: "heart beats",
-        data: [60, 65, 67, 75, 79, 60, 60]
-    },
-];
 
 const HeartRate = () => {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <Paper elevation={3}>
-            <Typography className={classes.root}>Heart Beats</Typography>
-            <Chart options={options} series={series} chart={chart} type="line" />
-        </Paper>
-    );
+  const date = ["01/09/2021 9:00", "02/09/2021 13:00", "03/09/2021 6:00", "04/09/2021 15:00", "05/09/2021 15:00", "06/09/2021 19:00", "07/09/2021 20:00"];
+  const options = {
+    chart: {
+      id: "basic-bar",
+    },
+    annotations: {
+      yaxis: [{
+        y: 65,
+        borderColor: '#00E396',
+        label: {
+          borderColor: '#00E396',
+          style: {
+            color: '#fff',
+            background: '#00E396'
+          },
+          text: 'Your resting HR'
+        }
+      }],
+      xaxis: [{
+        x: 3,
+        x2: 4,
+        fillColor: '#ff9393',
+        label: {
+          text: 'Irregularity'
+        }
+      }]
+    },
+    xaxis: {
+      categories: Array.from(Array(7).keys()),
+    },
+    tooltip: {
+      x: {
+        formatter: (s) => date[s - 1],
+      },
+    },
+    chart: {
+      toolbar: {
+        tools: {
+          reset: false,
+          pan: false
+        },
+      },
+    },
+  };
+
+  const chart = {
+    toolbar: {
+      tools: {
+        reset: false
+      }
+    }
+  }
+  const series = [
+    {
+      name: "heart beats",
+      data: [60, 65, 67, 75, 76, 60, 60]
+    },
+  ];
+
+  return (
+    <Paper elevation={3}>
+      <Typography className={classes.root}>Heart Rate</Typography>
+      <Chart options={options} series={series} chart={chart} type="line" />
+    </Paper>
+  );
 };
 
 export default HeartRate;
