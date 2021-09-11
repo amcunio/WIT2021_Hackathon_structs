9f8716a579af898a7e6dc50efbee98c2206eb1e6
Sam Shi <yunzeshi@outlook.com>
2021-09-11 20:50:07 +1000

    fixed position of the footer

--- src/components/PatientDashboard/Dashboard.jsx
+++ src/components/PatientDashboard/Dashboard.jsx
@@ -38,59 +38,8 @@ const useStyles = makeStyles((theme) => ({
     root: {
         display: "flex",
     },
-    toolbar: {
-        paddingRight: 24, // keep right padding when drawer closed
-    },
-    toolbarIcon: {
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "flex-end",
-        padding: "0 8px",
-        ...theme.mixins.toolbar,
-    },
-    appBar: {
-        zIndex: theme.zIndex.drawer + 1,
-        transition: theme.transitions.create(["width", "margin"], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-        }),
-    },
-    appBarShift: {
-        marginLeft: drawerWidth,
-        width: `calc(100% - ${drawerWidth}px)`,
-        transition: theme.transitions.create(["width", "margin"], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-    },
-    menuButton: {
-        marginRight: 36,
-    },
-    menuButtonHidden: {
-        display: "none",
-    },
     title: {
         flexGrow: 1,
-    },
-    drawerPaper: {
-        position: "relative",
-        whiteSpace: "nowrap",
-        width: drawerWidth,
-        transition: theme.transitions.create("width", {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-    },
-    drawerPaperClose: {
-        overflowX: "hidden",
-        transition: theme.transitions.create("width", {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-        }),
-        width: theme.spacing(7),
-        [theme.breakpoints.up("sm")]: {
-            width: theme.spacing(9),
-        },
     },
     appBarSpacer: theme.mixins.toolbar,
     content: {
@@ -111,6 +60,13 @@ const useStyles = makeStyles((theme) => ({
     fixedHeight: {
         height: 240,
     },
+    footer: {
+      position: "absolute",
+      width: "100%",
+      left: 0,
+      bottom: 0,
+      margin: "1rem 0",
+    }
 }));
 
 export default function Dashboard() {
@@ -152,7 +108,7 @@ export default function Dashboard() {
                 </Paper>
               </Grid>
             </Grid>
-            <Box pt={4}>
+            <Box pt={4} className={classes.footer} >
               <Copyright />
             </Box>
           </Container>
